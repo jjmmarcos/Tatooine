f7c786bd996cf7fd4012ce6464d5446cf2a10809
jjmmarcos <javierbulevar8@gmail.com>
2021-11-04 11:57:16 +0100

    Admin and User Authentication

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -67,6 +67,44 @@
 										</li>
 									</ul>
 								</li>
+								@if(Route::has('login'))
+									@auth
+										@if(Auth::user()->utype === 'ADM')
+											<li class="menu-item menu-item-has-children parent" >
+												<a title="My Account" href="#">My Account ({{Auth::user()->name}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+												<ul class="submenu curency" >
+													<li class="menu-item" >
+														<a title="Dashboard" href="{{ route('admin.dashboard') }}">Dashboard</a>
+													</li>
+													<li class="menu-item">
+														<a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+													</li>
+													<form id="logout-form" method="POST" action="{{ route('logout') }}">
+														@csrf														
+													</form>
+												</ul>
+											</li>
+										@else
+											<li class="menu-item menu-item-has-children parent" >
+												<a title="My Account" href="#">My Account ({{Auth::user()->name}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+												<ul class="submenu curency" >
+													<li class="menu-item" >
+														<a title="Dashboard" href="{{ route('user.dashboard') }}">Dashboard</a>
+													</li>
+													<li class="menu-item">
+														<a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+													</li>
+													<form id="logout-form" method="POST" action="{{ route('logout') }}">
+														@csrf														
+													</form>
+												</ul>
+											</li>
+										@endif
+									@else
+										<li class="menu-item" ><a title="Register or Login" href="{{route('login')}}">Login</a></li>
+										<li class="menu-item" ><a title="Register or Login" href="{{route('register')}}">Register</a></li>
+									@endif
+								@endif
 							</ul>
 						</div>
 					</div>
