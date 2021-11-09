d68423bfeb15c7e7023e50d95654e9e89cdb4eff
jjmmarcos <javierbulevar8@gmail.com>
2021-11-10 00:25:02 +0100

    Admin Add New Category

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
+use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -53,4 +54,5 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function(){
 Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function(){
     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
     Route::get('/admin/categories', AdminCategoryComponent::class)->name('admin.categories');
+    Route::get('/admin/category/add', AdminAddCategoryComponent::class)->name('admin.addcategory');
 });
