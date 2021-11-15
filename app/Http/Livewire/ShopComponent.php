8ae718185f37c3471cd198eeb34c56209c1565d8
jjmmarcos <javierbulevar8@gmail.com>
2021-11-15 17:44:51 +0100

    Remove Product from Wishlist

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -38,6 +38,19 @@ class ShopComponent extends Component
         $this->emitTo('whislist-count-component','refreshComponent');
     }
 
+    public function removeFromWishlist($product_id)
+    {
+        foreach(Cart::instance('wishlist')->content() as $witem)
+        {
+            if($witem->id == $product_id)
+            {
+                Cart::instance('wishlist')->remove($witem->rowId);
+                $this->emitTo('whislist-count-component','refreshComponent');
+                return;
+            }
+        }
+    }
+
     use WithPagination;
     public function render()
     {
