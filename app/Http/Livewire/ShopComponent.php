277896ebd42faae94775aa61b1e76ded0da57a46
jjmmarcos <javierbulevar8@gmail.com>
2021-11-15 17:31:05 +0100

    Auto Refresh Wishlist and Cart Count

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -27,14 +27,15 @@ class ShopComponent extends Component
     }
     public function store($product_id,$product_name,$product_price)
     {
-        Cart::instance('cart')->add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
+        Cart::instance('cart')->add($product_id, $product_name,1, $product_price)->associate('App\Models\Product');
         session()->flash('success_message','Item added in Cart');
         return redirect()->route('product.cart');
     }
 
     public function addToWishlist($product_id,$product_name,$product_price) 
     {
-        Cart::instance('wishlist')->add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
+        Cart::instance('wishlist')->add($product_id, $product_name,1, $product_price)->associate('App\Models\Product');
+        $this->emitTo('whislist-count-component','refreshComponent');
     }
 
     use WithPagination;
