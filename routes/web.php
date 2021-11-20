027d2cc4103f8f2c2e035cfe53bc50ddfa8bcb7a
jjmmarcos <javierbulevar8@gmail.com>
2021-11-20 20:19:24 +0100

    Admin Show Order Details

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ use App\Http\Livewire\Admin\AdminCouponsComponent;
 use App\Http\Livewire\Admin\AdminAddCouponComponent;
 use App\Http\Livewire\Admin\AdminEditCouponComponent;
 use App\Http\Livewire\Admin\AdminOrderComponent;
+use App\Http\Livewire\Admin\AdminOrderDetailsComponent;
 
 
 use Illuminate\Support\Facades\Route;
@@ -93,4 +94,5 @@ Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function(){
     Route::get('/admin/coupon/edit/{coupon_id}',AdminEditCouponComponent::class)->name('admin.editcoupon');
 
     Route::get('/admin/orders',AdminOrderComponent::class)->name('admin.orders');
+    Route::get('/admin/orders/{order_id}',AdminOrderDetailsComponent::class)->name('admin.orderdetails');
 });
